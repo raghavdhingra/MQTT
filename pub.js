e62fb79efb78f4013f004c5cbe8cd4f3fffa91f1
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-05-13 17:56:35 +0530

    publisher init

---
+++ pub.js
@@ -0,0 +1,11 @@
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://localhost:1234');
+const topic = 'RAGHAV';
+const message = 'YOYO';
+
+client.on('connect', () => {
+  setInterval(() => {
+    client.publish(topic, message);
+    console.log('Message sent! ', message);
+  }, 5000);
+});
